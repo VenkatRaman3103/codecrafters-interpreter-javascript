3fbae924579e99e2a769be37e428d953e6f270c3
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-07-09 08:34:52 +0530

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,12 +1,13 @@
 import fs from "fs";
 
-const args = process.argv.slice(2); // Skip the first two arguments (node path and script path)
+const args = process.argv.slice(2);
 
 if (args.length < 2) {
     console.error("Usage: ./your_program.sh tokenize <filename>");
     process.exit(1);
 }
 
+// command
 const command = args[0];
 
 if (command !== "tokenize") {
@@ -14,12 +15,23 @@ if (command !== "tokenize") {
     process.exit(1);
 }
 
+// file path
 const filename = args[1];
 
 const fileContent = fs.readFileSync(filename, "utf8");
 
 if (fileContent.length !== 0) {
-    throw new Error("Scanner not implemented");
+    // tokenize
+    for (let cursor = 0; cursor < fileContent.length; cursor++) {
+        let char = fileContent[cursor];
+
+        if (char === "(") {
+            console.log("LEFT_PAREN ( null");
+        } else if (char === ")") {
+            console.log("RIGHT_PAREN ) null");
+        }
+    }
+    console.log("EOF  null");
 } else {
     console.log("EOF  null");
 }
